7efda5a8cffc770225d69514f95f2ef2972522e3
LAMI220 <114293581+LAMI220@users.noreply.github.com>
2023-03-18 10:44:27 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+/*
+int main() {
+    int n, factorial = 1;
+    cout << "Hola Mundo soy LAMI ";
+    cout << "Ingrese numero para calcular factorial";
+    cin>> n;
+    if(n>0){
+        for(int i = 1; i <=n; i++)
+        {
+            factorial = factorial * i;
+            cout << " " << i;
+        }
+        cout<<endl
+        <<"El factorial de "<<n<<"!es:"<< factorial;
+    }
+    else {
+        cout << "alerta no es posible mostrar el factorial";
+    }
+    return 0;
+}
+int main (){
+    int a,b=0,c=1,d=1;
+    cout<<"HOLA Soy LAMI" << endl;
+    cout <<"prubra a fibonacci adelante:"<<endl;
+    cout<< "ingrese el valor a saber: ";
+    cin>>a;
+    cout<< "           1";
+    for (int i=1; i< a; i++) {
+        d = b + c;
+        cout << d << " ";
+        b = c;
+        c = d;
+    }
+    return 0;
+    }
+int main (){
+    int numero;
+    bool primo=true;
+    cout <<"Hola Soy LAMI"<<endl;
+    cout << "El dia de hoy aprenderemos los numeros primo y no primos"<<endl;
+    cout <<"Ingrese un numero: ";
+    cin >>numero;
+
+    for(int i=2; i<numero; i++){
+        if(numero %2 == 0){
+            primo = false;
+        }
+    }
+    if (primo == true){
+        cout <<"El numero es primo";
+    }
+    else {
+        cout<<"El numero no es primo";
+    }
+ getch ();
+    return 0;
+}
+int main () {
+    cout << "HOLA Soy LAMI" << endl;
+    cout << "El dia de hoy aprendemos los numeros par e impar"<<endl;
+    int numero;
+    cout << "Escriba un numero ";
+    cin >> numero;
+    if (numero == 0) {
+        cout << "El numero es 0";
+    } else if (numero%2 == 0) {
+        cout << "El numero es par";
+    } else {
+        cout << "El numero es impar";
+    }
+    return 0;
+}
+int main (){
+    int num;
+    cout <<";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;"<<endl;
+    cout <<"                    LAMI                      "<<endl;
+    cout <<";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;"<<endl;
+    cout <<"El dia de hoy aprenderemos las tablas de multiplicar"<<endl;
+    do {
+        cout << "Ingrese un numero de 1 al 10: ";
+        cin >> num;
+    } while ((num< 1)|| (num>10));
+    for (int i=0; i<=10; i++)
+    {
+        cout << num<<" x "<< i <<" = " << num * i << endl;
+    }
+        return 0;
+    }*/
